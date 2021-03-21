d1a20202841e1614e709d7d2449834bafc2d5793
Dima_Skup <dimaskupejko@gmail.com>
2021-03-21 13:28:14 +0200

    Laravel test project #2

--- database/migrations/2021_03_21_104158_add_enabled_to_events_table.php
+++ database/migrations/2021_03_21_104158_add_enabled_to_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEventsTable extends Migration
+class AddEnabledToEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-        Schema::create('events', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('venue');
-            $table->string('city');
-            $table->text('description')->nullable();
-            $table->timestamps();
+        Schema::table('events', function (Blueprint $table) {
+            $table->boolean('enabled')->after('name');
         });
     }
 
@@ -30,6 +25,8 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('events');
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropColumn('enabled');
+        });
     }
 }
