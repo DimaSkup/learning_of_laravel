cface91f0d117ac0e7da752f33c7716899f3ab5b
Dima_Skup <dimaskupejko@gmail.com>
2021-06-08 18:32:32 +0300

    A laravel test project #23

--- app/Event.php
+++ app/Event.php
@@ -101,6 +101,11 @@ class Event extends Model
                 ->withTimestamps();
     }
 
+    public function comments()
+    {
+        return $this->morphMany('App\Comment', 'commentable');
+    }
+
 
 
     protected static function boot()
